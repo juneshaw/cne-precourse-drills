148aea1e972b6347c7db8a0cb9767b63c6d12ec4
June Shaw <june.shaw@me.com>
2017-03-04 16:00:31 -0700

    Address class

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,6 +3,7 @@ package com.galvanize;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import main.java.com.galvanize.Address;
 
 public class Application {
 
@@ -18,6 +19,12 @@ public class Application {
         );
 
         System.out.println(builder.toJson(object));
+
+        demoAddress();
     }
 
+    private static void demoAddress() {
+        Address address = new Address("16 Laniuma Loop", "Honolulu", "HI", "96818");
+        System.out.println(address);
+    }
 }
